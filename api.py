6f3e9f74899e53a3162ce89c8ce9a43edc5453fa
Nick Mertens <nick@thecrypt.co.nz>
2017-04-14 23:45:36 +1200

    Add Flask and some simple endpoints

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
+from flask import Flask
+from flask import jsonify
 import requests
 
-baseUri = "https://the-crypt-1047.firebaseio.com/"
-jsonUri = ".json"
-pageUri = baseUri + "page/" + jsonUri
-postUri = baseUri + "post/" + jsonUri
+baseUri = 'https://the-crypt-1047.firebaseio.com/'
+jsonUri = '.json'
+pageUri = baseUri + 'page/' + jsonUri
+postUri = baseUri + 'post/' + jsonUri
 
 
 def getAllPosts():
@@ -53,3 +55,39 @@ pageList = getList(pages)
 postList = getList(posts)
 
 keywords = getKeywords(posts)
+
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found'
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+
+@app.route('/pages', methods=['GET'])
+def getPages():
+    return jsonify(pages)
+
+
+@app.route('/posts', methods=['GET'])
+def getPosts():
+    return jsonify(posts)
+
+
+@app.route('/post/<postid>', methods=['GET'])
+def getPost(postid):
+    if postid in posts:
+        return jsonify(posts[postid])
+    else:
+        return not_found()
+
+
+if __name__ == '__main__':
+    app.run()
